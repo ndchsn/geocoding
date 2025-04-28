71892ab4fa42029fe44807e2e0a1b194a515c45d
Andi Muhammad Ichsan Jalaluddin <andi.jalaluddin@multipolar.com>
2025-04-28 14:14:38 +0700

    update embed googlemap

--- app.py
+++ app.py
@@ -1,27 +1,37 @@
-import os
-import requests
 from flask import Flask, render_template, request
+import requests
+import os
 
 app = Flask(__name__)
 
-GOOGLE_API_KEY = os.environ.get("GOOGLE_API_KEY")  # Set di Cloud Run atau .env
+GOOGLE_API_KEY = os.getenv('GOOGLE_API_KEY')  # atau langsung hardcode buat testing
 
-@app.route('/')
+@app.route('/', methods=['GET'])
 def index():
-    return render_template('index.html')
+    return render_template('index.html', lat=None, lng=None)
 
 @app.route('/geocode', methods=['POST'])
 def geocode():
     location = request.form.get('location')
-    url = f'https://maps.googleapis.com/maps/api/geocode/json?address={location}&key={GOOGLE_API_KEY}'
-    response = requests.get(url)
-    result = response.json()
-    if result['status'] == 'OK':
-        latlng = result['results'][0]['geometry']['location']
-        return render_template('index.html', lat=latlng['lat'], lng=latlng['lng'], location=location)
+    if location:
+        endpoint = f'https://maps.googleapis.com/maps/api/geocode/json?address={location}&key={GOOGLE_API_KEY}'
+        response = requests.get(endpoint)
+        if response.status_code == 200:
+            result = response.json()
+            if result['status'] == 'OK':
+                lat = result['results'][0]['geometry']['location']['lat']
+                lng = result['results'][0]['geometry']['location']['lng']
+                formatted_address = result['results'][0]['formatted_address']  # 🔥 ini dia
+                return render_template('index.html', lat=lat, lng=lng, location=location, address=formatted_address)
+            else:
+                error = f"Lokasi tidak ditemukan: {result['status']}"
+                return render_template('index.html', error=error)
+        else:
+            error = "Gagal mengambil data dari Google Maps API."
+            return render_template('index.html', error=error)
     else:
-        return render_template('index.html', error="Lokasi tidak ditemukan.")
+        error = "Silakan masukkan nama lokasi."
+        return render_template('index.html', error=error)
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 8080))
-    app.run(host="0.0.0.0", port=port, debug=True)
+    app.run(debug=True)
